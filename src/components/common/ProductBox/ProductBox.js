7314109e9ec6a6572f6a1b8d27fc2f7c31b83c64
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-09-09 20:44:10 +0200

    Add to compare functionality - store

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -10,63 +10,85 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { toggleToCompare } from '../../../redux/productsRedux';
 
-const ProductBox = ({ name, price, promo, priceOld, stars, favorite, toCompare, id }) => (
-  <div className={styles.root}>
-    <div
-      className={styles.photo}
-      style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/images/products/${id}.jpeg)`,
-        backgroundSize: 'cover',
-      }}
-    >
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
-      </div>
-    </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button className={clsx(favorite && styles.active)} variant='outline'>
-          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-        </Button>
-        <Button className={clsx(toCompare && styles.active)} variant='outline'>
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-        </Button>
-      </div>
-      {priceOld && (
-        <div className={styles.priceold}>
-          <Button noHover variant='light'>
-            $ {priceOld}
+const ProductBox = ({
+  name,
+  price,
+  promo,
+  priceOld,
+  stars,
+  favorite,
+  toCompare,
+  id,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleCompare = e => {
+    e.preventDefault();
+    dispatch(toggleToCompare(id));
+  };
+
+  return (
+    <div className={styles.root}>
+      <div
+        className={styles.photo}
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/images/products/${id}.jpeg)`,
+          backgroundSize: 'cover',
+        }}
+      >
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant='small'>Quick View</Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
           </Button>
         </div>
-      )}
-      <div className={styles.price}>
-        <Button noHover variant='small'>
-          $ {price}
-        </Button>
+      </div>
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button className={clsx(favorite && styles.active)} variant='outline'>
+            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+          </Button>
+          <Button className={clsx(toCompare && styles.active)} variant='outline'>
+            <FontAwesomeIcon icon={faExchangeAlt} onClick={handleCompare}>
+              Add to compare
+            </FontAwesomeIcon>
+          </Button>
+        </div>
+        {priceOld && (
+          <div className={styles.priceold}>
+            <Button noHover variant='light'>
+              $ {priceOld}
+            </Button>
+          </div>
+        )}
+        <div className={styles.price}>
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 ProductBox.propTypes = {
   children: PropTypes.node,
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,31 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* action name creator */
+const reducerName = 'cart';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+const TOGGLE_TO_COMPARE = createActionName('TOGGLE_TO_COMPARE');
+
+/* action creators */
+// export const toggleToCompare = payload => ({ payload, type: TOGGLE_TO_COMPARE });
+
+export const toggleToCompare = payload => ({ payload, type: TOGGLE_TO_COMPARE });
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case TOGGLE_TO_COMPARE: {
+      if (statePart.filter(product => product.toCompare === true).length <= 4) {
+        return statePart.map(product =>
+          product.id === action.payload
+            ? { ...product, toCompare: !product.toCompare }
+            : product
+        );
+      }
+    }
+
     default:
       return statePart;
   }
