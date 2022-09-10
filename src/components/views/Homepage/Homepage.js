c4eab1f04888de28ef36b2a705e9844046d26436
Adrian Gasiorek <adrian.gasiorek@icloud.com>
2022-09-10 20:31:54 +0200

    Add promoted component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,9 +5,11 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import PromotedBox from '../../features/PromotedBox/PromotedBox';
 
 const Homepage = () => (
   <div className={styles.root}>
+    <PromotedBox />
     <FeatureBoxes />
     <NewFurniture />
   </div>
