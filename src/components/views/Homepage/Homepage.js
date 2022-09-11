0db3911d3f33a8ef3ff821f43ae505a6ae07f799
Adam Kwidzyński <adamkwidzynski@gmail.com>
2022-09-11 21:07:40 +0200

    Add Brands component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,18 +1,15 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-
 import styles from './Homepage.module.scss';
-
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import BrandsBar from '../../layout/BrandsBar/BrandsBar';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
     <NewFurniture />
+    <BrandsBar />
   </div>
 );
-
-// Homepage.propTypes = {};
 
 export default Homepage;
