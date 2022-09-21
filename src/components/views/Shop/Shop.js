bb92ec3d8da36abc9e074e381348ffedf125995b
Piotr Tańcula <tanculapiotr@gmail.com>
2022-09-21 18:23:16 +0200

    the containers for the shop page are ready

---
+++ src/components/views/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <>
+      <div className='container-fluid text-center p-0 m-0'>shop</div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-9 text-center'>Product List</div>
+          <div className='col-3 text-center'>Filters</div>
+        </div>
+      </div>
+      <div className='container-fluid text-center p-0 m-0'>Brands</div>
+    </>
+  );
+};
+
+export default Shop;
